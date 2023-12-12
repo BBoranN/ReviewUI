5e5fe86ae6cfae54e6d6b37455e43f9764c57aaa
BBoranN <92054300+BBoranN@users.noreply.github.com>
2023-12-12 23:36:40 +0300

    route

--- ReviewSite/src/main.ts
+++ ReviewSite/src/main.ts
@@ -1,5 +1,20 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router"
+
+
+const routes=[
+    {path:'/', },
+
+]
+
+/* const router= VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+}) */
+
+
+
 
 createApp(App).mount('#app')
